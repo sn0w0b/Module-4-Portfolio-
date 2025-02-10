05f39e8098ccd3d9104e9b99164d61f21de4ee13
sn0w0b <167842670+sn0w0b@users.noreply.github.com>
2025-02-10 06:56:08 -0600

    Add files via upload
    Made changes to all files and provided comments in the code about what I did change.

---
+++ time.py
@@ -0,0 +1,12 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+
+finalTimeInt = (currentTimeInt + waitTimeInt) % 24
+print(finalTimeInt)
+
+#changed to correct variables
+#such as currentTimeStr, waitTimeStr, and finalTimeInt 
+#and use modulo (% 24)to ensure the result stays within a 24-hour clock
---
+++ time2.py
@@ -0,0 +1,10 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of nours to wait?")
+
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = (time + wait_time) %24
+print(time_when_alarm_go_off)
+#corrected spelling in wait_time and 
+#use modulo (% 24)to ensure the result stays within a 24-hour clock
